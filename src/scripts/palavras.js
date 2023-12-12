df0955390c4a09e04c617c3f0286a0340186ab3e
Denisson Pereira Santos <denisson159@gmail.com>
2023-12-12 20:14:35 -0300

    up

--- src/scripts/palavras.js
+++ src/scripts/palavras.js
@@ -1,6 +1,7 @@
 const palavrasChaves = {
     google: 'https://www.google.com',
     github: 'https://github.com/DenissonPereira/Cifra-Magica',
+    about: 'https://denissonpereira.github.io/Cifra-Magica/about',
     
 };
 
