797b9a29d6a7ed714b75919fdcdcdd1c2c7ae25c
Denisson Pereira Santos <denisson159@gmail.com>
2023-12-13 11:29:00 -0300

    up

--- src/components/sounds/pages/BandaRaioSolar/sounds/Dolores/Doloresmusics.jsx
+++ src/components/sounds/pages/BandaRaioSolar/sounds/Dolores/Doloresmusics.jsx
@@ -18,6 +18,9 @@ const Doloresmusics = () => {
 
   return (
     <div>
+        <hr />
+        <p>INTRODUÇÃO: {acordes[0]} {acordes[1]} {acordes[2]} {acordes[4]} </p>
+        <br />
         <p>{acordes[0]}</p>
         <p>Dolores flor morena de Madri</p>
         <p>{acordes[1]}</p>
@@ -27,6 +30,16 @@ const Doloresmusics = () => {
         <p>{acordes[3]}</p>
         <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{acordes[4]}</p>
         <p>Em uma tarde de sol</p>
+        <br />
+        <p>{acordes[0]}</p>
+        <p>Tu bailas na minha alma como louca</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{acordes[5]}</p>
+        <p>De castanholas e mantilha</p>
+        <p>{acordes[3]}</p>
+        <p>O um beijo teu na minha boca</p>
+        <p>{acordes[5]} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {acordes[0]}</p>
+        <p>Sob um luar de Sevilha</p>
+        <hr />
         <button onClick={trocarTom}>1/2 Tom</button>
         <button onClick={originalTom}>Original</button>
     </div>
